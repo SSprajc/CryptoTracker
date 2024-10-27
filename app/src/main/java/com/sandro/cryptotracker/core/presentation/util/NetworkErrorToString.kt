ca0a6b817c158604971a28f6508bd5e3d5eb59fe
SSprajc <ssprajc@racunarstvo.hr>
2024-10-27 17:43:02 +0100

    init commit / entire project

---
+++ app/src/main/java/com/sandro/cryptotracker/core/presentation/util/NetworkErrorToString.kt
@@ -0,0 +1,17 @@
+package com.sandro.cryptotracker.core.presentation.util
+
+import android.content.Context
+import com.sandro.cryptotracker.R
+import com.sandro.cryptotracker.core.domain.util.NetworkError
+
+fun NetworkError.toString(context: Context) : String {
+    val id =  when(this) {
+        NetworkError.REQUEST_TIMEOUT -> R.string.error_request_timeout
+        NetworkError.TOO_MANY_REQUESTS -> R.string.error_too_many_requests
+        NetworkError.NO_INTERNET -> R.string.error_no_internet
+        NetworkError.SERVER_ERROR -> R.string.error_unknown
+        NetworkError.SERIALIZATION -> R.string.error_serialization
+        NetworkError.UNKNOWN -> R.string.error_unknown
+    }
+    return context.getString(id)
+}
---
+++ app/src/main/java/com/sandro/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -0,0 +1,70 @@
+package com.sandro.cryptotracker.crypto.presentation.coin_list
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sandro.cryptotracker.crypto.presentation.coin_list.components.CoinListItem
+import com.sandro.cryptotracker.crypto.presentation.coin_list.components.previewCoin
+import com.sandro.cryptotracker.ui.theme.CryptoTrackerTheme
+
+@Composable
+fun CoinListScreen(
+    modifier: Modifier = Modifier,
+    state: CoinListState,
+    onAction: (CoinListAction) -> Unit
+) {
+    if (state.isLoading) {
+        Box(
+            modifier = modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(state.coins) { coinUi ->
+                CoinListItem(
+                    coinUi = coinUi,
+                    onClick = {
+                        onAction(CoinListAction.OnCoinClick(coinUi))
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                HorizontalDivider()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CoinListScreenPreview() {
+    CryptoTrackerTheme {
+        CoinListScreen(
+            state = CoinListState(
+                coins = (1..100).map {
+                    previewCoin.copy(id = it.toString())
+                }
+            ),
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background),
+            onAction = {}
+        )
+    }
+}
